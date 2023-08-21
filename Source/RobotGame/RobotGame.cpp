8e88f87edebfea1b461ae1ecd567502c62668ff8
SatoArata <satoarata0326@gmail.com>
2023-08-21 14:22:25 +0900

    初期コミット

---
+++ Source/RobotGame/RobotGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RobotGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RobotGame, "RobotGame" );
---
+++ Source/RobotGame/RobotGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RobotGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTGAME_API ARobotGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
